dd1b348a83780f4f4eeefdc9c6f0b3c0b04e2d13
Emanoel Lopes <emanoel.lopes@luizalabs.com>
2021-04-17 13:18:10 -0300

    test(e2e): favorites [wip]

--- cypress/integration/Home/favorites.spec.js
+++ cypress/integration/Home/favorites.spec.js
@@ -1,70 +1,30 @@
-context('Show only favorites', () => {
-  beforeEach(() => {
+context('[Seleção de favoritos]', () => {
+  before(() => {
     cy.visit('/');
+    cy.clearLocalStorage();
   });
 
-  it('should show an Alert informing that you don`t have favorites selected', () => {
-    cy
-      .wait(2000)
-      .get('[data-testid="msh--show-only-favorites"]')
-      .click();
-
-    cy
-      .get('[data-testid="msh--alert-1"]')
-      .should('have.text', 'Você não tem nenhum favorito selecionado!');
-
-    cy
-      .get('[data-testid="msh--show-only-favorites"]')
-      .click();
+  describe('Nenhum favorito selecionado', () => {
+    it('caso existam favoritos selecionados, deve exibir o alerta com a mensagem "Você não tem nenhum favorito selecionado!"', () => {
+      cy.wait(500);
+      cy.get('[data-testid="msh--show-only-favorites"]').click();
+      cy.get('[data-testid="msh--alert-1"]').should('have.text', 'Você não tem nenhum favorito selecionado!');
+      cy.get('[data-testid="msh--show-only-favorites"]').click();
+    });
   });
 
-  it('should show only selected favorites', () => {
-    cy
-      .get('[data-testid="msh--hero-1009165"]')
-      .click();
+  describe('Selecionar favoritos', () => {
+    it.only('deve exibir os favoritos após selecioná-los e exibir somente favoritos', () => {
+      cy.wait(500);
+      
+      cy.get('[data-testid="msh--hero-1009148"]').click();
+      cy.get('[data-testid="msh--hero-1010870"]').click();
 
-    cy
-      .get('[data-testid="msh--hero-1009220"]')
-      .click();
+      cy.get('[data-testid="msh--show-only-favorites"]').click();
 
-    cy
-      .get('[data-testid="msh--show-only-favorites"]')
-      .click();
-
-    cy.get('[data-testid="msh--heroes-list"] > li[data-is-favorite="true"] span')
-      .first()
-      .should('have.text', 'Avengers');
-
-    cy.get('[data-testid="msh--heroes-list"] > li[data-is-favorite="true"] span')
-      .eq(1)
-      .should('have.text', 'Captain America');
-  });
-
-  it('should show persisted favorites after page reload', () => {
-    cy
-      .get('[data-testid="msh--hero-1009165"]')
-      .click();
-
-    cy
-      .get('[data-testid="msh--hero-1009220"]')
-      .click();
-
-    cy
-      .get('[data-testid="msh--show-only-favorites"]')
-      .click();
-
-    cy
-      .reload()
-      .wait(2000);
-
-    cy
-      .get('[data-testid="msh--heroes-list"] > li[data-is-favorite="true"] span')
-      .first()
-      .should('have.text', 'Avengers');
-
-    cy
-      .get('[data-testid="msh--heroes-list"] > li[data-is-favorite="true"] span')
-      .eq(1)
-      .should('have.text', 'Captain America');
+      cy.get('[data-testid="msh--heroes-list"] > li span').first().should('have.text', 'Absorbing Man');
+      cy.get('[data-testid="msh--heroes-list"] > li span').last().should('have.text', 'Ajaxis');
+    });
   });
 });
+
